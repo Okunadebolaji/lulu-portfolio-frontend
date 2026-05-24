6066473458fba9e5744492ae7085fdd9563166ef
Okunade Habeeb <okunadehabeeb34@gmail.com>
2026-05-24 11:48:07 +0100

    Fix environments folder spelling

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -3,7 +3,7 @@ import { isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { environment } from '../../../enviroments/enviroment';
+import { environment } from '../../../environments/enviroment';
 
 export interface LoginRequest {
   email: string;
